cc3769c111450e36a53f68663c51a1838e4bbdbc
Justin Shepherd <justin.l.shepherd@gmail.com>
2020-05-07 12:27:05 -0500

    Initial commit

---
+++ app/src/main/java/com/raywenderlich/android/spacingout/MainActivity.kt
@@ -0,0 +1,29 @@
+
+package com.raywenderlich.android.spacingout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.raywenderlich.android.spacingout.images.ImagesFragment
+import com.raywenderlich.android.spacingout.lookup.LookupFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    val images = ImagesFragment()
+    supportFragmentManager.beginTransaction().replace(R.id.content, images).commit()
+    nav_view.setOnNavigationItemSelectedListener { menuItem ->
+      when (menuItem.itemId) {
+        R.id.images -> {
+          supportFragmentManager.beginTransaction().replace(R.id.content, images).commit()
+        }
+        R.id.lookup -> {
+          val lookup = LookupFragment()
+          supportFragmentManager.beginTransaction().replace(R.id.content, lookup).commit()
+        }
+      }
+      true
+    }
+  }
+}
